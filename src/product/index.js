fe64c417fd12c4a954cc69212cb9c9c5124ba1e0
KimJaewoo <kljw01124@gm.gist.ac.kr>
2023-04-24 02:43:52 +0900

    semi-finished

--- src/product/index.js
+++ src/product/index.js
@@ -2,17 +2,17 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constants.js";
+import dayjs from "dayjs";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   useEffect(function () {
     axios
-      .get(
-        `https://072f1b6c-ff17-4526-a87a-dc955c97fb8a.mock.pstmn.io/products/${id}`
-      )
+      .get(`${API_URL}/products/${id}`)
       .then(function (result) {
-        setProduct(result.data);
+        setProduct(result.data.product);
       })
       .catch(function (error) {
         console.log(error);
@@ -26,7 +26,7 @@ function ProductPage() {
   return (
     <div>
       <div id="image-box">
-        <img src={"/" + product.imageUrl} />
+        <img src={`${API_URL}/${product.imageUrl}`} />
       </div>
       <div id="profile-box">
         <img src="/images/icons/avatar.png" />
@@ -35,8 +35,10 @@ function ProductPage() {
       <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price}원</div>
-        <div id="creadtedAt">2023년 4월 22일</div>
-        <div id="description">{product.description}</div>
+        <div id="creadtedAt">
+          {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+        </div>
+        <pre id="description">{product.description}</pre>
       </div>
     </div>
   );
